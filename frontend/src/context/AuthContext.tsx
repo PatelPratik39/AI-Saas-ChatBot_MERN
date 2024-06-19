e3564a1d302c85822403d879d4dec3bad40ceb2e
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-19 13:26:53 -0500

    create useAuth using Context API

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+type User = {
+    name: string;
+    email: string;
+};
+type UserAuth = {
+    isLoggedIn: boolean;
+    user: User | null;
+    login: (email: string, password: string) => Promise<void>;
+    signup: (name: string, email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<UserAuth | null>(null);
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // once login functionality ready
+    useEffect(() => {
+        // fetch if the user's coockie are valid then skip login
+    }, []);
+
+    const login = async (email: string, password: string) => { }
+    const signup = async (name: string, email: string, password: string) => { }
+    const logout = async () => { }
+
+    const value = {
+        user,
+        isLoggedIn,
+        login,
+        logout,
+        signup,
+    };
+    return <AuthContext.Provider value={value}></AuthContext.Provider>
+}
+
+export const useAuth = () => useContext(AuthContext);
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div>
-      <h1> Hello AI Chat bot</h1>
+      <h1> Hello </h1>
     </div>
   );
 }
